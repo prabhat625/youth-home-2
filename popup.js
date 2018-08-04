84ad9f93e190ce0b24082dc0c02f53dd8d285bbe
prabhat625 <horseprabhat625@gmail.com>
2018-08-04 08:19:53 -0700

    2nd change

---
+++ popup.js
@@ -0,0 +1,6 @@
+function popup(){
+	window.alert("Note: The content in this website is not officially or anyhow related to sos childrens villages this website is just an experiment of a student");
+}
+
+window.addEventListener('DOMContentLoaded', popup ,false);
+window.addEventListener('Load', popup ,false);
